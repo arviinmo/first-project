0c267bd500b3ea89da7641149a366149157cbbb8
Arvin mostafaei <Arvinmostafaeii@gmail.com>
2021-12-03 19:00:16 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log('hello 5');
+/*
+console.log('hello 5');
+console.log('hello 5');
+console.log('hello 5');
+ */
+console.log('hello 5');
+console.log('hello 5');
+let a = 5;
+let b = 6;
+let c = a + b;
+console.log(c);
